65208e3b8f4aa04df976d3f38ec5d00f69437fa4
chlehd0509 <chlehd0509@gmail.com>
2018-10-22 22:44:03 -0700

    PopupApp commit

---
+++ inc/popup2.h
@@ -0,0 +1,19 @@
+#ifndef __popup2_H__
+#define __popup2_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "popup2"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.popup2"
+#endif
+
+#endif /* __popup2_H__ */
